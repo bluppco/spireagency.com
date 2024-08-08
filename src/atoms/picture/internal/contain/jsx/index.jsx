cfe5eaca8500b12cf8216562935ce658eabfa848
Shiva pandey <shivapandey9898@gmail.com>
2024-08-08 13:17:58 +0530

    feat: atom picture contain jsx

---
+++ src/atoms/picture/internal/contain/jsx/index.jsx
@@ -0,0 +1,18 @@
+const PictureContainJSX = ( { alternative_text, source } ) => {
+
+    return (
+
+        <picture className="w-full h-full">
+            <source media="(max-width: 375px)" srcSet={ source } />
+            <source media="(max-width: 640px)" srcSet={ source } />
+            <img
+                alt={ alternative_text }
+                className="w-full h-full object-contain"
+                src={ source }
+            />
+        </picture>
+
+    )
+    
+}
+export default PictureContainJSX
